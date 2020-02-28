a3e5b1e36c854da56ad0d6efde48c4f6d87c9f6c
Jonathan Student <jonathanyun@berkeley.edu>
2020-02-28 12:39:05 -0800

    created player and started playerScript

---
+++ On Stolen Time/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    #region healthVars
+
+    #endregion
+
+    #region movemenentVars
+    public float moveSpeed;
+    private Rigidbody2D playerRB;
+    private Vector3 dir;
+    #endregion
+
+    #region attackVars
+
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        playerRB.velocity = movement * moveSpeed;
+
+        // makes player look at mouse
+        dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
