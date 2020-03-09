b52fda66cef70b4a3d72f7bef2e66beb2be33ec8
Jonathan Student <jonathanyun@berkeley.edu>
2020-03-09 12:11:40 -0700

    started BSP generation

--- On Stolen Time/Assets/Scripts/DungeonGengeratorScript.cs
+++ On Stolen Time/Assets/Scripts/DungeonGengeratorScript.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class DungeonGengeratorScript : MonoBehaviour
 {
-    public int dungeonWidth;
-    public int dungeonHeight;
-    public int maxRoomSize;
-    public int minRoomSize;
+    public int DungeonWidth;
+    public int DungeonHeight;
+    public int MaxRoomSize;
+    public int MinRoomSize;
+    public float MaxWidthHeightRatio;
 
+    public class SubDungeon
+    {
+        private SubDungeon _l;
+        private SubDungeon _r;
+        private Rect _rect;
+        
+
+
+        public SubDungeon(float roomRatio, Rect r)
+        {
+            _rect = r;
+            _l = null;
+            _r = null;
+        }
+
+        public void Split()
+        {
+
+        }
+
+        public bool IsLeaf()
+        {
+            return _l == null && _r == null;
+        }
+
+        public SubDungeon GetL()
+        {
+            return _l;
+        }
+        public SubDungeon GetR()
+        {
+            return _r;
+        }
+
+    }
+
+
+    #region unityFuncs
     // Start is called before the first frame update
     void Start()
     {
-        
+        SubDungeon root = new SubDungeon(MaxWidthHeightRatio, new Rect(0, 0, DungeonWidth, DungeonHeight));
+
+
     }
+    #endregion
     
 }
