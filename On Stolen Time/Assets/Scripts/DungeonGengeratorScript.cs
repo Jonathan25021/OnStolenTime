e6134dccdbe65602f50311c00b655d1946ae25b0
Jonathan Student <jonathanyun@berkeley.edu>
2020-03-08 19:55:08 -0700

    stuff1

---
+++ On Stolen Time/Assets/Scripts/DungeonGengeratorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonGengeratorScript : MonoBehaviour
+{
+    public int dungeonWidth;
+    public int dungeonHeight;
+    public int maxRoomSize;
+    public int minRoomSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+}
