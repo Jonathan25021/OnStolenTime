ef69f4dfce25257f81c4c3867592bd97c3f33088
sharanya204 <41498182+sharanya204@users.noreply.github.com>
2020-03-17 14:56:54 -0700

    naive implementation of weapons/attack

---
+++ On Stolen Time/Assets/Scripts/Sword.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Weapon
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        inuse = false;
+        weaponClass = 1;
+        damage = 20;
+        attackSpeed = 10;
+    }
+
+    public void AttackingWeapon()
+    {
+        //deal damage specific to swords
+    }
+}
