fb9845b54aedfd887ceaa7d1c2201d641506c333
Sean Sananikone <seans1337@berkeley.edu>
2020-04-07 11:07:42 -0700

    Chest opening script.

---
+++ On Stolen Time/Assets/Scripts/ChestOpen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestOpen : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Items to spawn.")]
+    private GameObject[] drops;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        float toSpawn = Random.Range(0, 1.0f);
+        int index = Mathf.RoundToInt(toSpawn * drops.Length);
+        GameObject dropped = drops[index];
+        Instantiate(dropped, this.transform);
+    }
+}
