1fcb494226c432679b554a5372f32c6a8d48ea82
Sean Sananikone <seans1337@berkeley.edu>
2020-04-14 17:39:18 -0700

    Chest spawns correctly.

--- On Stolen Time/Assets/Scripts/ChestOpen.cs
+++ On Stolen Time/Assets/Scripts/ChestOpen.cs
@@ -8,11 +8,16 @@ public class ChestOpen : MonoBehaviour
     [Tooltip("Items to spawn.")]
     private GameObject[] drops;
 
-    private void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
-        float toSpawn = Random.Range(0, 1.0f);
-        int index = Mathf.RoundToInt(toSpawn * drops.Length);
-        GameObject dropped = drops[index];
-        Instantiate(dropped, this.transform);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            float toSpawn = Random.Range(0, 1.0f);
+            int index = Mathf.RoundToInt(toSpawn * drops.Length);
+            GameObject dropped = drops[index];
+            Instantiate(dropped, collision.transform.position, collision.transform.rotation);
+            Destroy(gameObject);
+        }
     }
+
 }
