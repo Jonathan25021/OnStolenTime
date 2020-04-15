49ab11796277c60e2f32bfae846b95b7eb4ff635
Sean Sananikone <seans1337@berkeley.edu>
2020-04-14 17:45:53 -0700

    Chests work correctly now.

--- On Stolen Time/Assets/Scripts/ChestOpen.cs
+++ On Stolen Time/Assets/Scripts/ChestOpen.cs
@@ -8,15 +8,22 @@ public class ChestOpen : MonoBehaviour
     [Tooltip("Items to spawn.")]
     private GameObject[] drops;
 
+    [SerializeField]
+    [Tooltip("Opened sprite.")]
+    private Sprite opened;
+
+    private bool closed = true;
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && closed)
         {
             float toSpawn = Random.Range(0, 1.0f);
             int index = Mathf.RoundToInt(toSpawn * drops.Length);
             GameObject dropped = drops[index];
             Instantiate(dropped, collision.transform.position, collision.transform.rotation);
-            Destroy(gameObject);
+            gameObject.GetComponent<SpriteRenderer>().sprite = opened;
+            closed = false;
         }
     }
 
