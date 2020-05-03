4dbcd5f690eb40dd75ae153ef71bb4a20479eb8a
sharanya204 <41498182+sharanya204@users.noreply.github.com>
2020-05-03 12:45:06 -0700

    naive anubis + cat merge
    Hopefully

---
+++ On Stolen Time/Assets/LineOfSightProjectileAnubis.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineOfSightProjectileAnubis : MonoBehaviour
+{
+    public float attackDamage = 5;
+    public Transform ProjectileLocation; //from where the projectile will get released
+    public GameObject projectile;
+    private GameObject player;
+    private bool inRange;
+    float timer;
+    public float timeToReload;
+    public float fireSpeed;
+
+    List<GameObject> playerList;
+
+    void Awake()
+    {
+        playerList = new List<GameObject>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > timeToReload)
+        {
+            // check if there are any targets within range
+            if (playerList.Count > 0)
+            {
+                timer = 0;
+                //shoot at each player in range
+                foreach (GameObject obj in playerList)
+                {
+                    //calculate the trajectory vector 
+                    float x = obj.transform.position.x - transform.position.x;
+                    float y = obj.transform.position.y - transform.position.y;
+                    Vector2 FireDirection = new Vector2(x, y);
+                    FireDirection = FireDirection.normalized * fireSpeed;
+
+                    // Create the projectile and Access its Rigidbody to add force
+                    GameObject newProjectile = Instantiate(projectile, transform.position, transform.rotation);
+                    newProjectile.GetComponent<Rigidbody2D>().velocity = FireDirection;
+                    //If the projectile exists after 5 seconds, destroy it
+                    Destroy(newProjectile, 5);
+                }
+            }
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerList.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerList.Remove(collision.gameObject);
+        }
+
+    }
+
+    //private void Launchhairball()
+    //{
+    //    //animation goes here
+    //    ProjectileLocation = GetComponentInParent<Rigidbody2D>().transform;
+    //    //            bulletShoot.GetComponent<Rigidbody2D>().velocity = (player.transform.position - transform.position).normalized * bulletSpeed;
+    //    Instantiate(hairball, ProjectileLocation.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+    //}
+}
---
+++ On Stolen Time/Assets/Scripts/EnemyScripts/AnubisScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnubisScript : EnemyScript
+{
+    public float attackDamageThump = 40;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerScript>().takeDamage(attackDamageThump);
+        }
+    }
+}
