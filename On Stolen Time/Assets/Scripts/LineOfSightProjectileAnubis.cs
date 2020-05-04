91785add143918af1e7f539ac23022bedc702826
Jonathan Student <jonathanyun@berkeley.edu>
2020-05-04 03:29:56 -0700

    fixed lighting, added enemy sprites to lighting

--- On Stolen Time/Assets/Scripts/LineOfSightProjectileAnubis.cs
+++ On Stolen Time/Assets/Scripts/LineOfSightProjectileAnubis.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineOfSightProjectileAnubis : MonoBehaviour
+public class LineOfSightAnubisProjectile : MonoBehaviour
 {
     public float attackDamage = 5;
     public Transform ProjectileLocation; //from where the projectile will get released
